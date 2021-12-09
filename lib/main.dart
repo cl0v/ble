0b3e2a5b6858742308f77a122e850e98e19e21f6
Marcelo Viana <22938764+cl0v@users.noreply.github.com>
2021-12-09 09:12:40 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:buzzao_ble_protocol/buzzao_ble_protocol.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  BleApi api = BleApi();
+
+  BuzzaoBle? response;
+  Uint8List? uint;
+
+  close() async {
+    CloseCashierSend _closingSession = CloseCashierSend(
+      operatorId: '02df37bc-ef3e-4c4d-9027-a1efc4cfc102',
+    );
+    final data = Uint8List.fromList(jsonEncode(_closingSession).codeUnits);
+    final params = ProtocolParameters(
+      macAddress: '70:B3:D5:7B:12:1D',
+      timeout: 3000,
+      command: BuzzaoBleCommands.closeSession,
+    );
+    api.send(params, data);
+    await Future.delayed(Duration(seconds: 6));
+    api.send(
+      ProtocolParameters(
+          macAddress: '70:B3:D5:7B:12:1D',
+          timeout: 3000,
+          command: uint![1],
+          ack: true),
+      null,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const Text('Dados recebidos: '),
+            StreamBuilder<Uint8List>(
+              stream: api.onDataReceive,
+              initialData: Uint8List.fromList([]),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Text('Sem dados');
+                }
+                if (snapshot.data != null) {
+                  uint = snapshot.data;
+                  response = BuzzaoBleProtocol.result(snapshot.data);
+                  return Text(
+                    response.toString(),
+                  );
+                }
+                return const Text('Aguardando...');
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Abrir'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                api.disconnect();
+              },
+              child: Text('Desconectar'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Fechar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
